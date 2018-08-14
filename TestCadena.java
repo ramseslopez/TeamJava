de53055866da956ef3f08f56443c8d80e8097d25
Eric <giovanny.m.t@ciencias.unam.mx>
2018-08-14 12:50:15 -0500

    Estructura de pruebas unitarias

---
+++ TestCadena.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+/**
+ * 
+ */
+public class TestCadena{
+
+    @Test
+    public void testConstructor(){
+
+    }
+    @Test
+    public void testConcatenacion(){
+
+    }
+
+    @Test
+    public void testReversa(){
+
+    }
+
+    @Test
+    public void testPalindromo(){
+
+    }
+
+    @Test
+    public void testRepetidos(){
+
+    }
+
+    @Test
+    public void testQuitaEspacios(){
+        
+    }
+}
