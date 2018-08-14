e5e4513c8564595f98fe1a823aa428feb9365b3c
Eric <giovanny.m.t@ciencias.unam.mx>
2018-08-14 12:39:19 -0500

    Estructura de la clase

---
+++ Cadena.java
@@ -0,0 +1,47 @@
+/**
+ * Clase para realizar operaciones con cadenas
+ */
+public class Cadena{
+    private String cad;
+    /**
+     * 
+     */
+    public Cadena(String cad){
+        this.cad = cad;
+    }
+
+    /**
+     * 
+     */
+    public String concatenacion(){
+        return "";
+    }
+
+    /**
+     * 
+     */
+    public String reversa(){
+        return "";
+    }
+
+     /**
+      * 
+      */
+    public String palindromo(){
+        return "";
+    }
+
+      /**
+       * 
+       */
+    public String repetidos(){
+        return "";
+    }
+
+      /**
+       * 
+       */
+    public String quitaEspacios(){
+        return "";
+    }
+}
